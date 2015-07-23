e2958e4952d3b8880110202f830bf0e83b846dc4
raiseandfall <druidev+github@gmail.com>
2015-07-23 16:10:58 -0700

    Initial Commit

---
+++ JMSRangeSlider/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  JMSRangeSlider
+//
+//  Created by Matthieu Collé on 23/07/2015.
+//  Copyright © 2015 JohnMcNeil Studio. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
