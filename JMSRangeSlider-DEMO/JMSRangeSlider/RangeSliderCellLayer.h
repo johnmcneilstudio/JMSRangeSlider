beb1844f5f940ffefd620d385070140b8fa87468
raiseandfall <druidev+github@gmail.com>
2015-07-29 17:23:32 -0700

    [CONFIG] Added header files to be framework-ready

---
+++ JMSRangeSlider-DEMO/JMSRangeSlider/RangeSliderCellLayer.h
@@ -0,0 +1,13 @@
+//
+//  RangeSliderCellLayer.h
+//  JMSRangeSlider
+//
+//  Created by Matthieu Collé on 29/07/2015.
+//  Copyright © 2015 JohnMcNeil Studio. All rights reserved.
+//
+
+#ifndef RangeSliderCellLayer_h
+#define RangeSliderCellLayer_h
+
+
+#endif /* RangeSliderCellLayer_h */
---
+++ JMSRangeSlider-DEMO/JMSRangeSlider/RangeSliderTrackLayer.h
@@ -0,0 +1,13 @@
+//
+//  RangeSliderTrackLayer.h
+//  JMSRangeSlider
+//
+//  Created by Matthieu Collé on 29/07/2015.
+//  Copyright © 2015 JohnMcNeil Studio. All rights reserved.
+//
+
+#ifndef RangeSliderTrackLayer_h
+#define RangeSliderTrackLayer_h
+
+
+#endif /* RangeSliderTrackLayer_h */
